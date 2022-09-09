12647acee9182e757864f7ae3464f4934e821e53
Eerik Anttila <eerik.anttila@woolman.io>
2022-09-09 10:10:32 +0300

    first

---
+++ server/routes.js
@@ -0,0 +1,113 @@
+const cheerio = require('cheerio');
+const fetch = require('axios');
+const iconv = require('iconv-lite');
+const Bottleneck = require('bottleneck');
+
+const limiter = new Bottleneck({
+  minTime: 0
+});
+
+module.exports = app => {
+	
+app.get('/tori/:area', function(req, res) {
+res.header("Access-Control-Allow-Origin", "*");
+const area = req.params.area;
+let url;
+switch (area) {
+  case 'keski-suomi':
+    url = "https://www.tori.fi/keski-suomi?q=&cg=0&w=1&st=g&ca=7&l=0&md=th";
+    break;
+  case 'varsinais-suomi':
+    url = "https://www.tori.fi/varsinais-suomi?q=&cg=0&w=1&st=g&ca=16&l=0&md=th";
+    break;
+  case 'pirkanmaa':
+    url = "https://www.tori.fi/pirkanmaa?q=&cg=0&w=1&st=g&ca=11&l=0&md=th";
+    break;
+  default:
+    url = "https://www.tori.fi/keski-suomi?q=&cg=0&w=1&st=g&ca=7&l=0&md=th";
+}
+
+fetch(url, { responseType: 'arraybuffer'})
+    .then(function(response) {
+		const data = iconv.decode(response.data, 'ISO-8859-1');
+        return data
+    })
+    .then(function(html) {
+		const urls = [];
+		//const table = Object.values(cheerio('.list_mode_thumb > a > .desc_flex > .ad-details-left > .li-title', html))
+		const table = Object.values(cheerio('.list_mode_thumb > a', html))
+		
+		table.map((obj, indx) => {
+			if (indx < 5) {
+				const url = obj.attribs.href
+				const item = Object.values(cheerio('a > .desc_flex > .ad-details-left > .li-title', obj))
+				const name = item[0].children[0].data
+				if (item && obj.attribs) {
+					//console.log(item[0].children[0].data)
+					const object = {
+						"name": name,
+						"url": url
+					}
+					urls.push(object)
+				}				
+			}
+		})
+		//console.log(urls)
+
+		return Promise.all(urls.map(limiter.wrap((obj, indx) => {
+			return getDetails(obj)
+		}))).then(function(values) {
+			//console.log(values);
+			res.setHeader('Content-Type', 'application/json');
+			res.end(JSON.stringify(values));
+		 }, reason => {
+			console.error(reason.message); // Error!
+		});
+		return
+    })
+    .catch(function(err) {  
+        console.log('Failed to fetch page: ', err);  
+    });
+	
+	const getDetails = (obj) => {
+    return new Promise((resolve, reject) => {
+	fetch(obj.url, { responseType: 'arraybuffer'})
+    .then(function(response) {
+		const data = iconv.decode(response.data, 'ISO-8859-1');
+        return data
+    })
+    .then(function(html) {
+		const descDiv = Object.values(cheerio('.body', html))
+		if (descDiv[0].children[2]) {
+			const desc = descDiv[0].children[2].data
+			const descParsed = desc.replace(/\n+/g, ' ').trim()
+			obj.desc = descParsed			
+		}
+		const image = Object.values(cheerio('#main_image', html))
+		const imageUrl = image[0].attribs.src
+		obj.image = imageUrl
+		const thumbnails = Object.values(cheerio('.thumbnails > .thumbs_on_left > #thumbs > li > span > img', html))
+		const imagesArr = [];
+		thumbnails.map((thumbnail, indx) => {
+			if (thumbnail.name == 'img') {
+				const fullImgUrl = thumbnail.attribs.src.replace("thumbs", "big");
+				imagesArr.push(fullImgUrl)
+			}
+		})
+		obj.images = imagesArr
+		resolve(obj)
+    })
+    .catch(function(err) {  
+        reject(err);
+    })
+  })
+  };
+})
+  
+  app.get('/second/:format', function (req, res) {
+	  const format = req.params.format
+		res.setHeader('Content-Type', 'application/json');
+		res.end(JSON.stringify(format));
+  })
+	
+};
---
+++ src/components/Area.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Area = ({ areaName, items, setItems }) => {
+	React.useEffect(() => {
+		if (items) {
+			setProducts(items);
+		}
+		setCurrentArea(areaName);
+	}, [areaName, items])
+	
+	const [currentArea, setCurrentArea] = React.useState('');
+	const [products, setProducts] = React.useState([]);
+	
+  return (<div> 
+  <h2 style={{marginBottom: '0px'}}>{currentArea}</h2>
+  <p style={{margin: '0px'}}>Viimeisimmät 5 tavaraa:</p>
+	{products.map((product, idx) => {
+	return(
+	<div key={idx}>
+	 <div>
+		<div className="row" style={{marginTop: '15px'}}>
+		<div className="col" style={{height: '170px', maxWidth: '800px', padding: '5px', borderLeft: '1px solid lightGray', borderRight: '1px solid lightGray', borderTop: '1px solid lightGray', backgroundColor: '#f2f2f2', boxShadow: '1px 1px 4px #808080', marginBottom: '5px'}}>
+		<a href={product.url}>
+		<div style={{display: 'inline-block', border: '1px solid black', height: '100px', width: '100px', backgroundImage: "url("+product.image+")", backgroundSize: 'contain', backgroundColor: 'gray', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}></div></a>
+		<div style={{maxWidth: '650px', marginLeft: '10px', display: 'inline-block', position: 'absolute', paddingRight: '30px'}}>		
+			<b>{product.name}</b><br/>
+			<p style={{margin: '0px', border: '0px solid black', minHeight: '60px', maxHeight: '60px', overflowY: 'scroll'}}>{product.desc}</p>
+			<a href={product.url}>Linkki sivulle</a>
+			<p style={{margin: '0px', display: 'inline', marginLeft: '2px'}}> {product.images.length} kuva(a):</p>
+		</div>
+
+		<div style={{marginTop: '5px'}}>
+		{product.images.map((img, idx) => {
+			return(
+			<a href={img} key={idx}><img src={img} style={{marginLeft: '2px', border: '1px solid black', height: '50px', width: '50px', display: 'inline-block'}}/></a>
+		)})}
+		</div>
+		
+		</div>
+		</div>
+	</div></div>)})}
+  </div>);
+}
+
+export default Area;
